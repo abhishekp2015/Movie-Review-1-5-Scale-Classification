d05d0e1663bb41d3ca72db4b751a9406d7adcb85
Achyut Shukla <achyut.usc@gmail.com>
2016-11-28 00:47:15 -0800

    print stmt add

--- Utils.py
+++ Utils.py
@@ -78,7 +78,9 @@ def buildFeatureList(featureObj):
 
 
 def printstats(p_dict):
+    fo = open('size_feature_vector' + '_output.txt', 'w')
 
     for i in range(1, 6):
-        print(i, ' ->', len(p_dict[i]))
+        #print(i, ' ->', len(p_dict[i]))
+        fo.write(i, ' ->', len(p_dict[i])
 
